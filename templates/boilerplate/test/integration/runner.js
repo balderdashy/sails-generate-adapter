b01a580df6bd4974e4e115491919503be5b5e84c
Mike McNeil <mike@balderdash.co>
2014-02-01 18:55:25 -0600

    Update runner.js

--- templates/boilerplate/test/integration/runner.js
+++ templates/boilerplate/test/integration/runner.js
@@ -1,12 +1,4 @@
 /**
- * Test runner dependencies
- */
-var util = require('util');
-var mocha = require('mocha');
-var log = new (require('captains-log'))();
-
-
-/**
  * Run integration tests
  *
  * Uses the `waterline-adapter-tests` module to
@@ -16,6 +8,14 @@ var log = new (require('captains-log'))();
  * are tested. (e.g. `queryable`, `semantic`, etc.)
  */
 
+
+/**
+ * Module dependencies
+ */
+
+var util = require('util');
+var mocha = require('mocha');
+var log = new (require('captains-log'))();
 var TestRunner = require('waterline-adapter-tests');
 var Adapter = require('../../');
 
